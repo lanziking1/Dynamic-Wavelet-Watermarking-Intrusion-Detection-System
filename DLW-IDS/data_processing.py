0cb6cfd023d64df45dc707c000c5948a9c47df41
LiChangqing <128276822+lanziking1@users.noreply.github.com>
2026-01-19 14:57:54 +0800

    Add files via upload

---
+++ DLW-IDS/data_processing.py
@@ -0,0 +1,118 @@
+import random
+import numpy as np
+import pandas as pd
+from scipy.stats import kurtosis, skew
+
+# 提取特征
+def get_feature(signal, feature_label):
+    spectral_flatness = np.std(signal) / np.abs(np.mean(signal)) if np.abs(np.mean(signal)) != 0 else 0
+    # 计算均值、方差、偏度和峰度
+    mean_value = np.mean(signal)
+    variance = np.var(signal)
+    skewness = skew(signal)
+    kurtosis_value = kurtosis(signal,
+                              fisher=False)
+    # 进行映射,获取卷积之后的信息
+    use_signal = [spectral_flatness, mean_value, variance, skewness, kurtosis_value]
+    result = convolve(signal=use_signal, kernel=feature_label)
+    return result
+
+
+# 利用卷积将label of marking 信息隐藏的嵌入到数据中
+def convolve(signal, kernel, stride=2):
+    result = []
+    n = len(signal)
+    k = len(kernel)
+
+    for i in range(0, n - k + 1, stride):
+        # 提取输入中的一部分来与卷积核相乘
+        window = signal[i:i + k]
+        # 计算卷积
+        conv_value = sum(w * k for w, k in zip(window, kernel))
+        # 添加到结果列表中
+        result.append(conv_value)
+    return result
+
+
+# 进行位置嵌入，保证插入的水印信息，不相临,Flag决定是否再次随机确定插入的位置
+def embed_elements(data, elements_to_embed, flag=True, pos1=None, pos2=None):
+    if flag == True:
+        if len(data) < 3:
+            raise ValueError("The data list must have at least 3 elements to embed [1, 0] non-adjacently.")
+
+        pos1 = random.randint(0, len(data) - 2)
+        if pos1 == 0:
+            pos2_range = range(2, len(data) + 1)
+        elif pos1 == len(data) - 2:
+            pos2_range = range(0, pos1)
+        else:
+            pos2_range = list(range(pos1 + 2, len(data) + 1)) + list(range(0, pos1))
+
+        pos2 = random.choice(pos2_range)
+
+    if pos1 < pos2:
+        new_data = data[:pos1] + [elements_to_embed[0].tolist()] + data[pos1:pos2] + [
+            elements_to_embed[1].tolist()] + data[pos2:]
+    else:
+        new_data = data[:pos2] + [elements_to_embed[0].tolist()] + data[pos2:pos1] + [
+            elements_to_embed[1].tolist()] + data[pos1:]
+    return new_data, flag, pos1, pos2
+
+
+
+def get_data():
+    # 定义时间步，time_step
+    time_step = 3
+    # Label of marking
+    feature_label = [1, 0, 1]
+    # 保存所有数据和标签
+    db_data = []
+    db_y = []
+    # 读取数据集
+    DoS_Data = pd.read_csv("  ")
+    DoS_Group = DoS_Data.groupby("CAN ID")
+    for name, group in DoS_Group:
+        group = group[['Timestamp', 'CAN ID', ' DLC', 'DATA0', 'DATA1', 'DATA2',
+                       'DATA3', 'DATA4', 'DATA5', 'DATA6', 'DATA7', 'Label']].values
+        for i in range(time_step, group.shape[0] % 100):
+            # 按顺序获取每一列数据，获取输入数据
+            temp = group[i - time_step:i, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+            # 保存处理的样本，并进行转置
+            temp_data = []
+            # 保证每一个样本中插入的顺序一致
+            flag = True
+            pos1 = 0
+            pos2 = 0
+            for j in range(temp.shape[1]):
+                result = get_feature(temp[:, j], feature_label=feature_label)
+                new_data, flag, pos1, pos2 = embed_elements(temp[:, j].tolist(), result, flag=flag,
+                                                            pos1=pos1, pos2=pos2)
+                temp_data.append(new_data)
+                print(new_data)
+                flag = False
+            # 将样本转置过后保存到db_data中
+            db_data.append(np.array(temp_data).T)
+            print(db_data[0])
+            # 进行打标签，如果label为1，就是注入数据相反就是正常数据
+            if np.any(group[i - time_step:i, [11]] == 1):
+                db_y.append([1, 0, 1, 0, 1])
+
+
+            else:
+                db_y.append([0, 1, 1, 0, 1])
+
+    # 数据集转化为numpy
+    db_data = np.array(db_data)
+    db_y = np.array(db_y)
+    # 数据集打乱
+    np.random.seed(116)
+    np.random.shuffle(db_data)
+    np.random.seed(116)
+    np.random.shuffle(db_y)
+    # 进行维度变化
+    print("db_data.shape before reshape:", db_data.shape)
+    db_data = np.reshape(db_data, (db_data.shape[0], time_step + 2, db_data.shape[2]))
+    print("db_data.shape:", db_data.shape)
+    return db_data, db_y
+
+# get_data()
---
+++ DLW-IDS/model&run.py
@@ -0,0 +1,174 @@
+import itertools
+import os
+import time
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from attention import Attention
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, GRU, Flatten, Dropout
+from deal_data_marking import get_data
+
+X, y = get_data()
+
+# 划分训练集和测试集
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=True)
+# 要统计的值
+value_to_count = 0
+# 使用条件表达式和np.count_nonzero统计值出现的次数
+count = np.count_nonzero(y_test[:, 0] == value_to_count)
+print(f"The value {value_to_count} appears {count} times in the array.")
+
+# 要统计的值
+value_to_count = 1
+# 使用条件表达式和np.count_nonzero统计值出现的次数
+count = np.count_nonzero(y_test[:, 0] == value_to_count)
+print(f"The value {value_to_count} appears {count} times in the array.")
+
+model = Sequential([
+    GRU(40, activation='tanh', return_sequences=True),
+    Dropout(0.2),
+    Attention(),
+    Flatten(),
+    Dense(40, activation='relu'),
+    Dropout(0.2),
+    Dense(20, activation='relu'),
+    Dropout(0.2),
+    Dense(5)
+])
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+              loss='mean_squared_error')  # 损失函数用均方误差
+# 加载预训练权重
+checkpoint_save_path = "./checkpoint/GRU.ckpt"
+if os.path.exists(checkpoint_save_path + '.index'):
+    print('-------------load the model-----------------')
+    model.load_weights(checkpoint_save_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_save_path,
+                                                 save_weights_only=True,
+                                                 save_best_only=True)
+
+history = model.fit(x_train, y_train, batch_size=2048, epochs=200, validation_data=(x_test, y_test), validation_freq=1,
+                    callbacks=[cp_callback])
+model.summary()
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()
+
+################## predict ######################
+# 测试集输入模型进行预测
+start_time = time.time()
+y_pre = model.predict(x_test)
+end_time = time.time()
+use_time = end_time-start_time
+
+# pd.DataFrame(y_pre, columns=['normal', 'DoS']).to_csv('save_result/result.csv')
+# 获取攻击分类结果
+y_pre_attack = y_pre[:, 0:2]
+y_real_attack = y_test[:, 0:2]
+y_pre_attack = np.argmax(y_pre_attack, axis=1)
+y_real_attack = np.argmax(y_real_attack, axis=1)
+
+result = confusion_matrix(y_real_attack, y_pre_attack)
+
+
+# 绘制混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=True, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    - cm : 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize : True:显示百分比, False:显示个数
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("显示百分比：")
+        np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
+        print(cm)
+    else:
+        print('显示具体数字：')
+        print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes)
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes, rotation=45)
+    plt.ylim(len(classes) - 0.5, -0.5)
+    fmt = '.5f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('confuse3.jpg', dpi=800)
+    plt.show()
+
+
+attack_types = ['0', '1']
+plot_confusion_matrix(result, classes=attack_types, normalize=False, title='confusion matrix')
+
+accuracy = accuracy_score(y_real_attack, y_pre_attack)
+# 计算macro-average F1分数
+f1_macro = f1_score(y_real_attack, y_pre_attack, average='macro')
+
+# 计算精确度和召回率
+# 'macro' 表示计算每个类别的指标，然后取平均值
+precision_macro = precision_score(y_real_attack, y_pre_attack, average='macro')
+recall_macro = recall_score(y_real_attack, y_pre_attack, average='macro')
+
+# print("全局Accuracy:", accuracy)
+print("Precision:", precision_macro)
+print("Recall:", recall_macro)
+print("F1 Score:", f1_macro)
+print("全局Accuracy:", accuracy)
+print('用时', use_time / y_test.shape[0])
+# 绘制编码评估指标，将评估指标进行保存，方便评估
+# 获取水印分类结果
+y_pre_masking = y_pre[:, 2:]
+y_real_masking = y_test[:, 2:]
+df1 = pd.DataFrame(y_pre_masking, columns=['pre_a', 'pre_b', 'pre_c'])
+df2 = pd.DataFrame(y_real_masking, columns=['real_a', 'real_b', 'real_c'])
+pd.concat([df1,df2],axis=1).to_csv('result.csv')
+# 创建一个新的图形
+plt.figure()
+# 设置一个颜色，例如红色
+color = 'red'
+# 循环遍历数组的每一列，并绘制它，但使用不同的线型
+for i, column in enumerate(y_pre_masking.T):  # 使用.T转置数组以按列迭代
+    linestyle = ['-', '--', ':'][i]  # 为每条线选择不同的线型
+    plt.plot(column, label=f'Column {i + 1}', color=color, linestyle=linestyle)
+
+# 设置一个颜色，例如红色
+color = 'yellow'
+# 循环遍历数组的每一列，并绘制它，但使用不同的线型
+for i, column in enumerate(y_real_masking.T):  # 使用.T转置数组以按列迭代
+    linestyle = ['-', '--', ':'][i]  # 为每条线选择不同的线型
+    plt.plot(column, label=f'Column {i + 1}', color=color, linestyle=linestyle)
+# 设置图例
+plt.legend()
+
+# 设置标题和坐标轴标签（可选）
+plt.title('Real Label of Marking')
+plt.xlabel('Index')
+plt.ylabel('Value')
+
+# 显示图形
+plt.show()
+# Precision: 1.0
+# Recall: 1.0
+# F1 Score: 1.0
+# 全局Accuracy: 1.0
+# 用时 0.0001220130937060543
